edd47175829a6f0d0dc4b4cc51221db1285a1e47
cmoraes5 <moraescaio0502@gmail.com>
2022-03-11 11:05:38 -0300

    :up: A just animation fixed

--- script.js
+++ script.js
@@ -19,39 +19,7 @@
 
 
 // Exemplo:
-// - No exemplo será utilizado uma API que vai puxar uma lista de produtos
-
-
-// //=> A função fetch retorna uma promise, ou seja uma promessa, um código que ainda está vindo
-// fetch('https://jsonplaceholder.typicode.com/todos/1'
-// // Na arrow function, se retornamos apenas 1 item, ñ precisamos de: ;{} return
-// ).then(r => //---> R de response
-// //==> O then basicamente vai executar algo, quando arequisição acontecer
-// //=> É necessário transformar a resposta de acordo com oque veio do item, se for HTML -> json
-// // se for um JSON -> json
-// // - Para transformar tbm demora um pouvo pôs tamvém é assinc
-//     r.json()
-// ).then(jsonBody => {
-// //=> Inserindo no #app o primeiro item da lista de produtos dentro do jsonBody, pós transformado
-//     document.querySelector("#app").innerHTML = jsonBody[0].title
-//     console.log(jsonBody) //==> Isso é o objt final, transformado, podendo fazer oque quiser
-// }); 
-// // Dizer oque fazer com a requisição
-
-// const data = {
-// }
-
-// // Para escrever, precisamos primeiro declarar a url,
-// // e dps um objt com opções -> Dentro dele o mthod: Post
-// fetch("https://ranekapi.origamid.dev/wp-json/api/usuario", {
-//     method: "POST",
-//     headers: {
-//         "Content-Type": "aplication/json"
-//     },
-//     body: JSON.stringify(data)
-// })
-
-
+// - No exemplo será utilizado uma API que vai puxar uma lista aleatória
 
 function fetchApiData() {
 
